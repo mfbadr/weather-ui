77f1315de1559c1e9e5b844be9f443df0333b0c0
Mikey Badr <mfbadr@gmail.com>
2014-07-31 19:38:54 -0500

    added api call and correct variable passing

---
+++ app/index.js
@@ -0,0 +1,57 @@
+/* jshint camelcase:false */
+'use strict';
+
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/static'));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', function (req, res){
+  res.render('form');
+});
+
+app.post('/', function (req, res){
+  // call wunderground api with zip, pass temp
+  var zip = req.body.zip;
+  var url = 'http://api.wunderground.com/api/2738df9035fbffa9/conditions/q/' + zip + '.json';
+  var temp;
+
+  function getTemp( url, cb){
+    request( url, function( error, response, body){
+        body = JSON.parse(body);
+        temp = body.current_observation.temp_f;
+        cb(temp);
+      });
+    }
+
+//  var storeTemp = function(x){
+//    temp = x;
+//  };
+
+  getTemp( url, function(x){
+    temp =  x;
+    console.log(temp);
+    res.render('weather', {temp:temp, zip:zip} );
+  });
+
+  
+  
+//  console.log(temp);
+//  console.log(zip);
+});
+
+var port = process.env.PORT;
+
+app.listen(port, function(){
+  console.log('Express is listening on PORT', port + '...');
+});
